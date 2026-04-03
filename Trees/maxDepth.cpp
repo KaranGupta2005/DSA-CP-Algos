6b75056c58f9ad47ca7846d100f16f9752385c7c
KaranGupta_2006 <guptakaran.port@gmail.com>
2026-04-03 19:04:01 +0530

    maxDepth and sameTree

---
+++ Trees/maxDepth.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdcpp.h>
+using namespace std;
+
+// Maximum Depth= maximum number of nodes from the root to any leaf
+/*
+Height of a node
+- check left subtree
+- check right subtree
+- take maximum value + 1 (for current node)
+
+- we have to process children info first
+- so it's a postorder traversal
+*/
+
+int maxDepth(TreeNode* root){
+    if(!root) return 0;
+
+    int leftHeight=maxDepth(root->left);
+    int rightHeight=maxDepth(root->right);
+
+    return max(leftHeight,rightHeight) + 1;
+}
+
+/*
+Same Tree
+- check if two trees are identical
+- check if values of nodes are equal
+- check if left subtrees are equal
+- check if right subtrees are equal
+*/
+bool sameTree(TreeNode* p , TreeNode* q){
+    if(!p && !q) return true;
+    if(!p || !q) return false;
+    if(p->val != q->val) return false;
+
+    return (p->left , q->left) && (p->right , q->right);
+}
