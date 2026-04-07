9db69e846bb6787ac65ec38e025df78f1ce15876
KaranGupta_2006 <guptakaran.port@gmail.com>
2026-04-07 18:18:36 +0530

    preorder2

--- Trees/Traversals/iterative_dfs.cpp
+++ Trees/Traversals/iterative_dfs.cpp
@@ -46,3 +46,94 @@ vector<int>inorder(TreeNode* root){
 
     return result;
 }
+
+/*
+- Root Left Right
+- Process the node immediately
+- then go to left
+- then go to right
+
+- firstly push right -> then left into the stack coz stack is a LIFO data structure 
+*/
+vector<int> preorder(TreeNode* root){
+    vector<int> result;
+    if(!root) return result;
+
+    stack<TreeNode*> st;
+    st.push(root);
+
+    while(!st.empty()){
+        TreeNode* node=st.top();
+        st.pop();
+        result.push_back(node->val);
+
+        if(node->right) st.push(node->right);
+        if(node->left) st.push(node->left);
+    }
+    
+    return result;
+}   
+
+/*
+- Left Right Root
+- we will be using two stacks
+- first stack is used to process the nodes ( reverse preorder logic)
+- second stack is used to store the nodes in reverse order
+*/
+vector<int> postorder(TreeNode* root){
+    vector<int> result;
+    if(!root) return result;
+
+    stack<TreeNode*> st1,st2;
+    st1.push(root);
+
+    while(!st1.empty()){
+        TreeNode* node=st1.top();
+        st1.pop();
+        st2.push(node);
+        
+        if(node->left) st1.push(node->left);
+        if(node->right) st1.push(node->right);
+    }
+    
+    while(!st2.empty()){
+        result.push_back(st2.top()->val);
+        st2.pop();
+    }
+    
+    return result;
+}
+
+/*
+- Uses a single stack only
+- but have to keep the track of lastvisited node
+*/
+vector<int> postorder2(TreeNode* root){
+    vector<int> result;
+    if(!root) return result;
+
+    TreeNode* lastVisited=nullptr;
+    stack<TreeNode*> st;
+
+    while(curr || !st.empty()){
+        while(curr){
+            st.push(curr);
+            curr=curr->left;
+        }
+
+        TreeNode* node=st.top();
+        if(node->right && lastVisited!=node->right){
+            curr=node->right;
+        }
+        else{
+            // process the node
+            result.push_back(node->val);
+            lastVisited=node;
+            st.pop();
+        }
+    }
+    
+    return result;
+}
+// Time Complexity: O(n)
+// Space Complexity: O(h) where h is the height of the tree
