109ee195e94f91228ba8a1915a4052d7e8922c53
KaranGupta_2006 <guptakaran.port@gmail.com>
2026-06-18 09:36:31 +0530

    right side view

---
+++ Trees/LevelOrderProcessing.cpp/RightSideView.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Right Side View
+- The answer vector should include the rightmost node of each level
+- BFS with level tracking
+- DFS : Visit right child first, then left child
+*/
+
+vector<int> rightSideView(TreeNode* root){
+
+    vector<int> ans;
+    if(!root) return ans;
+
+    queue<TreeNode*> q;
+    q.push(root);
+
+    while(!q.empty()){
+        int n=q.size();
+
+        for(int i=0 ; i<n ; i++){
+            auto node=q.front();
+            q.pop();
+
+            if(i==n-1) ans.push_back(node->val);
+            
+            if(node->left) q.push(node->left);
+            if(node->right) q.push(node->right);
+        }
+    }
+    
+    return ans;
+}
--- Trees/Traversals/bfs.cpp
+++ Trees/Traversals/bfs.cpp
@@ -33,39 +33,3 @@ vector<vector<int>> levelOrder(TreeNode* root){
 // Time Complexity: O(n)
 // Space Complexity: O(w) where w is the maximum width of the tree
 // BFS used to find the shortest path in unweighted graphs
-
-vector<vector<int>> zigzagLevelOrder(TreeNode* root){
-    vector<vector<int>> result;
-    if(!root) return result;
-
-    queue<TreeNode*> q;
-    q.push(root);
-
-    bool leftToRight = true;
-
-    while(!q.empty()){
-        int size=q.size(); // number of nodes in current level
-        vector<int> level(size,0);
-
-        for(int i=0;i<size;i++){
-            auto* top=q.front();
-            q.pop();
-
-            int idx=(leftToRight) ? i : size-1-i;
-            level[idx] = top->val;
-
-            if(top->left) q.push(top->left);
-            if(top->right) q.push(top->right);  
-        }
-        
-        
-        
-        result.push_back(level);
-        leftToRight = !leftToRight;
-    }
-    
-    return result;
-}
-// Level by level traversal with alternating direction
-// firstly left->right then right->left
-// instead of reversing after each level , we place the elements correctly
