3934760b46ad4a0444cbcdaf36b9dcd1a3064ed1
KaranGupta_2006 <guptakaran.port@gmail.com>
2026-08-24 12:25:12 +0530

    slow fast pointer

---
+++ 2 Pointers/Same Direction 2 Pointers.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+Same Direction /Fast-Slow Pointers
+- Here both pointers move in the same direction
+- Useful for finding patterns, cycles, or specific properties in a single pass
+- fast pointer explores the array , while slow maintains the valid / processed region
+*/
+
+/*
+Remove Duplicates
+- We are given an array and it is not possible to change the length of it 
+- So if there are k unique elements , then the first k elements should be unique
+- new length = slow + 1
+
+- below is the generalized code : even if are able to remove some elements and each element may appear atmost k times
+- then : if(slow<k || nums[fast] != nums[slow-k])
+*/
+int removeDuplicates(vector<int>& nums){
+    int slow=0;
+
+    for(int fast=0 ; fast<nums.size() ; fast++){
+        if(slow<1 || nums[fast] != nums[slow-1]){
+            nums[slow] = nums[fast];
+            slow++;
+        }
+    }
+    return slow;
+}
